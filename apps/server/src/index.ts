0b46a4a4bba8a10372927f0ceb83bdf234136f1b
arafatDU <bsse1435@iit.du.ac.bd>
2025-07-01 15:24:01 +0600

    httpServer created with typescript configuration

---
+++ apps/server/src/index.ts
@@ -0,0 +1,14 @@
+import http from 'http';
+
+
+async function init() {
+  const httpServer = http.createServer();
+  const PORT = process.env.PORT || 8000;
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+
+init();
