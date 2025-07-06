6cb6aa4ccee46b8d0a48b59aafb1fa1c4d14fb8b
arafatDU <bsse1435@iit.du.ac.bd>
2025-07-06 15:01:07 +0600

    redis added with the server pub sub

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,36 +1,59 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
 
+
+const pub = new Redis({
+  host: "",
+  port: 0,
+  username: "default",
+  password: "",
+});
+
+const sub = new Redis({
+  host: "",
+  port: 0,
+  username: "",
+  password: "",
+});
 
 class SocketService {
   private _io: Server;
 
   constructor() {
-    console.log("Initializing Socket service...");
+    console.log("Init Socket Service...");
     this._io = new Server({
       cors: {
-        origin: "*",
         allowedHeaders: ["*"],
+        origin: "*",
       },
     });
+    sub.subscribe("MESSAGES");
   }
 
   public initListeners() {
     const io = this.io;
-    console.log("Initializing Socket listeners...");
+    console.log("Init Socket Listeners...");
 
     io.on("connect", (socket) => {
-      console.log("New socket connected:", socket.id);
+      console.log(`New Socket Connected`, socket.id);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New Message Rec.", message);
+        // publish this message to redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+      });
+    });
 
-      socket.on("event:message", async ({message}: {message: string}) => {
-        console.log("Received message:", message);
-      })
-    })
+    sub.on("message", async (channel, message) => {
+      if (channel === "MESSAGES") {
+        console.log("new message from redis", message);
+        io.emit("message", message);
+      }
+    });
   }
 
   get io() {
     return this._io;
   }
 }
-
 
 export default SocketService;
